9b2cea0cafdaf54927f069a34b85e7f691a581f9
borgella <jmborgella@hotmail.com>
2016-12-05 22:10:48 -0500

    created json service, formated of movies html file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { JsonPService } from './services/app.jsonp.services';
+
 import { AppComponent } from './app.component';
 import { MoviesComponent } from './components/movies/movies.component';
 import { ConnexionComponent } from './components/connexion/connexion.component';
@@ -19,7 +21,7 @@ import { ConnexionComponent } from './components/connexion/connexion.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [JsonPService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
