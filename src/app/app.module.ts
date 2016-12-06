97538677996859dbb9841a8c96727008645a07ea
borgella <jmborgella@hotmail.com>
2016-12-05 21:25:06 -0500

    added components into appModule file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,15 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MoviesComponent } from './components/movies/movies.component';
+import { ConnexionComponent } from './components/connexion/connexion.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent,
+    ConnexionComponent
   ],
   imports: [
     BrowserModule,
