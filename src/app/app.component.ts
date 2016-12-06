35934c127cbf011c5c861e334b6d8dafa4d084b0
borgella <jmborgella@hotmail.com>
2016-12-05 22:24:35 -0500

    injected jsonP services into app.component.ts file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { JsonPService } from './services/app.jsonp.services';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  public constructor(private _jsonpService: JsonPService) {}
 }
--- src/app/services/app.jsonp.services.ts
+++ src/app/services/app.jsonp.services.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Jsonp } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
-export class JsonPService {}
+export class JsonPService {
+
+    api_key: string;
+
+    public constructor(private _jsonp: Jsonp) {
+        this.api_key = 'f4d3ed51e5f0e3ec6d93c4d735e9d599';
+    }
+}
