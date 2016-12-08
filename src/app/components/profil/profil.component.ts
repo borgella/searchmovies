b1aef2fcdf981a87a5b140bf4e2201cd45dd4237
borgella <jmborgella@hotmail.com>
2016-12-08 18:47:05 -0500

    added js file

--- src/app/components/profil/profil.component.ts
+++ src/app/components/profil/profil.component.ts
@@ -3,7 +3,7 @@ import { Auth0Service } from '../../services/app.auth0.services';
 @Component({
     selector: 'app-profil',
     templateUrl: './profil.component.html',
-    styleUrls: ['./profil.component.css']
+    styleUrls: ['./profil.component.css', 'font-awesome.min.css']
 })
 export class ProfilComponent {
     public constructor(private auth0: Auth0Service) {}
