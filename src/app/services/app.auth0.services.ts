744bcb0e9acf264c907bae0307397596bf051b08
borgella <jmborgella@hotmail.com>
2016-12-29 03:19:33 -0500

    profile set

--- src/app/services/app.auth0.services.ts
+++ src/app/services/app.auth0.services.ts
@@ -1,4 +1,4 @@
-import { Injectable }      from '@angular/core';
+import { Injectable } from '@angular/core';
 import { tokenNotExpired } from 'angular2-jwt';
 
 // Avoid name not found warnings
@@ -9,16 +9,17 @@ export class Auth0Service {
 
   // Configure Auth0
   lock = new Auth0Lock('76GAhDWCwIGkyzQmThiUFPK5kG9snP0S', 'borgella.auth0.com', {});
-  userProfile: Object;
+  userProfile = new Object();
   constructor() {
+    this.userProfile = JSON.parse(localStorage.getItem('profile'));
     this.lock.on('authenticated', (authResult) => {
       localStorage.setItem('id_token', authResult.idToken);
-      this.lock.getProfile(authResult.idToken, function(error: any, profile: Object){
+      this.lock.getProfile(authResult.idToken, function (error: any, profile: Object) {
         if (error) { return; }
         localStorage.setItem('profile', JSON.stringify(profile));
       });
+      this.userProfile = JSON.parse(localStorage.getItem('profile'));
     });
-    this.userProfile = JSON.parse(localStorage.getItem('profile'));
   }
 
   public login() {
@@ -31,6 +32,7 @@ export class Auth0Service {
 
   public logout() {
     localStorage.removeItem('id_token');
+    localStorage.removeItem('profile');
   };
 
 }
