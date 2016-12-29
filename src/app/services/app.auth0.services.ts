15e1003d9a39d6410b71383cdaa783d681832c05
borgella <jmborgella@hotmail.com>
2016-12-28 22:30:36 -0500

    setting up getUserInfo method in auth0service file

--- src/app/services/app.auth0.services.ts
+++ src/app/services/app.auth0.services.ts
@@ -3,16 +3,28 @@ import { tokenNotExpired } from 'angular2-jwt';
 
 // Avoid name not found warnings
 declare var Auth0Lock: any;
+declare var Auth0: any;
 
 @Injectable()
 export class Auth0Service {
 
   // Configure Auth0
   lock = new Auth0Lock('76GAhDWCwIGkyzQmThiUFPK5kG9snP0S', 'borgella.auth0.com', {});
+  auth0 = new Auth0({
+    domain: 'borgella.auth0.com',
+    clientID: '76GAhDWCwIGkyzQmThiUFPK5kG9snP0S',
+    callbackURL: 'https://borgella.github.io/searchmovies/',
+    responseType: 'token'
+  });
 
   constructor() {
     // Add callback for lock `authenticated` event
     this.lock.on('authenticated', (authResult) => {
+      this.auth0.getUserInfo(authResult.idToken, function(error, profile){
+        if (profile) {
+          console.log(profile);
+        }
+      });
       localStorage.setItem('id_token', authResult.idToken);
     });
   }
